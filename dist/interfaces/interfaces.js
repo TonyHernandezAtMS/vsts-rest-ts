ece6d6f16ed8c8279bdccfbd982f4af45bc7a932
Tony Hernandez <9831944+TonyHernandezAtMS@users.noreply.github.com>
2017-11-09 13:14:14 -0600

    Remove dist folder from git

--- dist/interfaces/interfaces.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FieldNames = {
-    Tags: "System.Tags",
-    Title: "System.Title",
-    History: "System.History",
-    ID: "System.Id"
-};
